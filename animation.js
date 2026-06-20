343562b4ef31685a71475f78ec9a2a7f5a6ba50b
anatoliy <makar01.02@mail.ru>
2026-06-20 15:54:13 +0800

    Добавлены анимации

---
+++ animation.js
@@ -0,0 +1,13 @@
+const blocks = document.querySelectorAll(".block");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+blocks.forEach(block => observer.observe(block));
